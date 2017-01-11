165513bf4a60b6fc5848c85c977c85a1f03e3a5c
David Robinson <dr6@sanger.ac.uk>
2017-01-11 10:19:02 +0000

    a52 add url to create ownerships as a batch

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -44,6 +44,58 @@ class OwnershipsController < ApplicationController
     @ownership.destroy
   end
 
+  # POST /ownerships/batch
+  # {
+  #   "ownership": [
+  #      {
+  #         "model_id": "xxxxxxxxxxx",
+  #         "model_type": "THING",
+  #         "owner_id": "jeff@jeff.com"
+  #      },
+  #      ...
+  #   ]
+  # }
+  def create_batch
+    # Batch upsert does not seem to be possible.
+    # Instead, separate out the creates and updates.
+    #   Batch update does not seem to be possible.
+    #   However, batch create is definitely possible.
+    modelidhash = {}
+    # I don't know what the expected behaviour is if the same model id is passed twice,
+    #  so abort if a duplicate is found.
+    params.require(:ownership).each do |item|
+      modelid = item.require(:model_id)
+      if modelidhash.has_key?(modelid)
+        error = {
+          status: 400,
+          message: "Repeated model id: #{modelid.inspect}"
+        }
+        return render json: error, status: :bad_request
+      end
+      modelidhash[modelid] = item.permit(:model_id, :model_type, :owner_id)
+    end
+
+    modelids = modelidhash.keys
+
+    Ownership.transaction do
+
+      # Find which of the model ids are already in the database and update them
+      Ownership.where(model_id: modelids).find_each do | ownership |
+        modelid = ownership.model_id
+        ownership.update_attributes!(modelidhash[modelid])
+        # Any that we have updated this way, we don't need to create in the next step
+        modelidhash.delete(modelid)
+      end
+      # For items that we still need to create, do them all as a batch
+      Ownership.create!(modelidhash.values) unless modelidhash.empty?
+      # Load the results for the response
+      @ownerships = Ownership.where(model_id: modelids)
+      
+    end # -- transaction
+
+    render json: @ownerships, status: :created
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_ownership
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   resources :ownerships, param: :model_id
+
+  post 'ownerships/batch' => 'ownerships#create_batch'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
